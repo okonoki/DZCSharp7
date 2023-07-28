7ccce4987368e89ee9bcbea37951f4b2f5082c3f
okonoki <okonokiss@gmail.com>
2023-07-28 13:20:20 +0500

    task02 is done

--- task02/Program.cs
+++ task02/Program.cs
@@ -1,1 +1,65 @@
-﻿
+﻿// Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+int GetNumber(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+
+int[,] GetArray(int rows, int columns)
+{
+    int[,] array = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+bool GetResultNumber(int[,] arr, int row, int column)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            if (row == i && column == j) 
+            {
+                Console.Write(arr[i, j]);
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+int rows = GetNumber("Введите количество строк массива: ");
+int columns = GetNumber("Введите количество столбцов массива: ");
+
+int[,] array = GetArray(rows, columns);
+PrintArray(array);
+
+int resRow = GetNumber("Введите индекс строки массива: ");
+int resColumn = GetNumber("Введите индекс столбца массива: ");
+if (GetResultNumber(array, resRow, resColumn)) Console.Write("Такого числа нет в массиве!");
